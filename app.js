b2ac8284b46110d680b53f4d8754adbac0d4f6a3
NamDell <namkhanh.nguyen@edu.sait.ca>
2020-08-30 12:11:23 -0600

    almost done

---
+++ app.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    console.log(scroll_position);
+    if (scroll_position > 450) {
+        header.style.backgroundColor = "#29323c";
+        console.log('dit chi m ');
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+})
